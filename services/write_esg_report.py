864744e4611b9cf0a2335c525f3e3393e1a7eaed
Paul P <85466221+pauljhp@users.noreply.github.com>
2024-08-19 20:28:02 +0800

    fixed bugs

--- services/write_esg_report.py
+++ services/write_esg_report.py
@@ -5,43 +5,49 @@ from agents import (
 )
 from settings import function_llm, llm
 from tasks import get_new_esg_review_task
-from tools import get_query_engine_tool, RetrievalTools, SearchTools
+from tools import (
+    get_query_engine_tool,
+    RetrievalTools,
+    SearchTools
+)
 from crewai_tools import DirectoryReadTool
 from tempfile import TemporaryDirectory
 from crewai import Crew, Agent, Task, Process
 from settings import (
-    set_global_configs, # TODO - change to a decorator
+    set_global_configs,  # TODO - change to a decorator
     langfuse_callback_handler,
     langfuse_handler,
     callback_manager
-    )
+)
 import os
 
 
 temp_dir = TemporaryDirectory()
 editor_tool = DirectoryReadTool(directory=temp_dir.name)
 
-def analyze_esg(company_info, async_exec: bool=False):
-    set_global_configs() # TODO - change to a decorator
-    retrieval_tool = RetrievalTools(
-        company_info=company_info, 
-        collection_name="dev_esg_reports")
-    # get_query_engine_tool(
-    #     company_info=company_info, 
-    #     collection_name="dev_esg_reports"
-    #     )
+
+def analyze_esg(company_info, async_exec: bool = False):
+    set_global_configs()  # TODO - change to a decorator
+    retrieval_tool = get_query_engine_tool(
+        company_info=company_info,
+        collection_name="dev_esg_reports"
+    )
+    # RetrievalTools(
+    #     company_info=company_info,
+    #     collection_name="dev_esg_reports")
+
     tools = [retrieval_tool] + [SearchTools.search_internet]
     supervisor = get_supervisor(coworkers=["editor", "esg_analyst"])
     esg_agent = get_esg_analyst(
         tools=tools
-        )
+    )
     editor = get_editor(tools=[editor_tool])
     tasks = get_new_esg_review_task(
-        company_info, 
-        tools=tools, 
+        company_info,
+        tools=tools,
         async_exec=async_exec,
         agent=esg_agent
-        )
+    )
     crew = Crew(
         agents=[esg_agent, editor],
         tasks=tasks,
@@ -49,7 +55,7 @@ def analyze_esg(company_info, async_exec: bool=False):
         process=Process.sequential,
         verbose=True,
         # manager_llm=function_llm,
-        # manager_agent=supervisor, 
+        # manager_agent=supervisor,
         memory=True,
         planning=False,
         planning_llm=function_llm,
@@ -58,9 +64,10 @@ def analyze_esg(company_info, async_exec: bool=False):
         # config={"callbacks": callback_manager},
         embedder={
             "provider": "azure_openai",
-            "config":{
+            "config": {
                 "model": 'text-embedding-ada-002',
-                "endpoint": os.getenv("AZURE_OPENAI_ENDPOINT"),
+                "api_base": os.getenv("AZURE_OPENAI_ENDPOINT"),
+                "api_key": os.getenv("AZURE_OPENAI_API_KEY"),
                 "deployment_name": os.getenv("DEFAULT_EMBEDDING_MODEL")
             }
         },
--- tools/report_tools.py
+++ tools/report_tools.py
@@ -1,4 +1,4 @@
-from crewai_tools import tool, BaseTool
+from crewai_tools import tool, BaseTool, LlamaIndexTool
 from pydantic import model_validator, BaseModel
 from settings import li_llm, llm
 from dbs import get_index, get_query_engine
@@ -8,64 +8,74 @@ from llama_index.core.vector_stores import (
     MetadataFilters,
     FilterOperator,
     FilterCondition
-    )
+)
 from llama_index.core.tools import (
-    QueryEngineTool, 
+    QueryEngineTool,
     ToolMetadata
-    )
+)
 from typing import List, Dict, Annotated
 
 
 class QueryEngineSchema(BaseModel):
-    question: Annotated[str, "The question you are tring to ask to the database. "]
+    question: Annotated[str,
+                        "The question you are tring to ask to the database. "]
+
 
 class QueryEngineResponse(BaseModel):
     response: Annotated[str, "The immediate response"]
-    sources: Annotated[str, "The source of the information, converted to string"]
+    sources: Annotated[str,
+                       "The source of the information, converted to string"]
+
 
 def get_query_engine_tool(
-        collection_name: str, 
+        collection_name: str,
         company_info: CompanyInfo,
-        filter_keys: List[str]=["sedol", "isin", "figi"]
-        ):
+        filter_keys: List[str] = ["sedol", "isin", "figi"]
+):
     filters = [
-            MetadataFilter(key=key, value=value) 
-            for key, value 
-            in company_info.model_dump().items()
-            if (value is not None and key in filter_keys)
-            ]
+        MetadataFilter(key=key, value=value)
+        for key, value
+        in company_info.model_dump().items()
+        if (value is not None and key in filter_keys)
+    ]
     index = get_index(collection_name=collection_name)
     if filters:
-            metadata_filters = MetadataFilters(
-                filters=filters,
-                condition=FilterCondition.AND
-            )
-            engine = index.as_query_engine(
-                llm=li_llm,
-                filters=metadata_filters
-            )
+        metadata_filters = MetadataFilters(
+            filters=filters,
+            condition=FilterCondition.AND
+        )
+        engine = index.as_query_engine(
+            llm=li_llm,
+            filters=metadata_filters
+        )
     else:
         engine = index.as_query_engine(llm=li_llm)
-    query_tool = QueryEngineTool(
-        query_engine=engine,
-        metadata=ToolMetadata(
-            name="Vector DB Retrieval Tool",
-            description=("Tool for retrieving information fromm the vector "
-                "store about a particular company. You inputs MUST be a string."),
-            fn_schema=QueryEngineSchema
-            )
-        ).to_langchain_tool()
+    LlamaIndexTool.from_query_engine(
+        engine,
+        name="Vector DB Retrieval Tool",
+        description=("Tool for retrieving information fromm the vector "
+                     "store about a particular company. You inputs MUST be a string."),
+    )
+# query_tool = QueryEngineTool(
+    #     query_engine=engine,
+    #     metadata=ToolMetadata(
+    #         name="Vector DB Retrieval Tool",
+    #         description=("Tool for retrieving information fromm the vector "
+    #             "store about a particular company. You inputs MUST be a string."),
+    #         fn_schema=QueryEngineSchema
+    #         )
+    #     ).to_langchain_tool()
     return query_tool
+
 
 class RetrievalTools(BaseTool):
     """Subclassing implementation of the retrieval tool"""
     name: str = "Vector DB Retrieval Tool"
     description: str = "Tool for retrieving information fromm the vector "
     "store about a particular company. You inputs MUST be a string."
-    filter_keys: List[str]=["sedol", "isin", "figi"]
+    filter_keys: List[str] = ["sedol", "isin", "figi"]
     company_info: CompanyInfo
     collection_name: str
-    
 
     @model_validator(mode="before")
     def setup_engine(cls, values):
@@ -80,11 +90,11 @@ class RetrievalTools(BaseTool):
         """Use this to query for information in the database. """
         # FIXME - seems to trigger error: This was the error: Error code: 403 - {'error': {'code': 'unsupported_country_region_territory', 'message': 'Country, region, or territory not supported', 'param': None, 'type': 'request_forbidden'}}
         filters = [
-            MetadataFilter(key=key, value=value) 
-            for key, value 
+            MetadataFilter(key=key, value=value)
+            for key, value
             in self.company_info.model_dump().items()
             if (value is not None and key in self.filter_keys)
-            ]
+        ]
         index = get_index(collection=self.collection_name)
         if filters:
             metadata_filters = MetadataFilters(
@@ -106,7 +116,7 @@ class RetrievalTools(BaseTool):
             return res
         else:
             res = QueryEngineResponse(
-                response="The query engine has been prepared. However, since you did not "\
+                response="The query engine has been prepared. However, since you did not "
                 "pass in a query, I will not return anything. Please pass a query",
                 sources="None")
             return res
