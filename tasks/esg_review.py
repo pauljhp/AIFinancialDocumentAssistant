90e84d4a8560cc9d1a562d37494960b6453cf60a
ppipxam <p.peng@impaxam.com>
2024-08-28 21:18:58 +0800

    fixed tool calling schema

--- tasks/esg_review.py
+++ tasks/esg_review.py
@@ -35,10 +35,12 @@ class NewESGReview:
     ):
         self.company = company_info
         self.task_descriptions = task_descriptions
-        self.expected_output = ("Detailed analysis of each point, together with your scoring "
+        self.expected_output = ("Detailed analysis of each point (including subpoints), together with your scoring "
                                 "and justification. You should write your answers in bullet point "
                                 "format, and clearly quote your sources, as well as give detailed "
-                                "justification. "
+                                "justification. Do no just give one answer, provide details to the answer "
+                                "and your through process. If the template asks for multiple aspects, "
+                                "try to cover all aspects in bullet points. "
                                 )
         self.async_exec = async_exec
         self.agent = agent  # TODO - bind tools with agents
